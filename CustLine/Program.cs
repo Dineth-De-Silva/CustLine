bcdb29c35c3a17bedbe20c8c5727ea7ed9d0b7b5
D. A. Dineth De Silva <dinethdes@gmail.com>
2021-04-03 22:38:42 +0530

    Initial commit

---
+++ CustLine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
